5f3ac9f96851ffe4d953cbbca2296b358d712756
charbelazzi584 <charbelazzi338@gmail.com>
2025-03-06 20:34:28 +0200

    changed

--- com/example/demo6/miniProject.java
+++ com/example/demo6/miniProject.java
@@ -283,6 +283,9 @@ public class miniProject extends Application {
                     errorMsg.setText("Please fill in all the required fields.");
                 }
 
+
+                
+
             });
         })
         ;}}
